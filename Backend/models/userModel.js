abf3b860a92b01d5f932759ced8f51254adef900
Obieda Akilan <obieda.jehad@gmail.com>
2023-03-04 12:19:27 +0300

    Make the schema of user and export the model of user

---
+++ Backend/models/userModel.js
@@ -0,0 +1,42 @@
+'use strict';
+const mongoose = require('mongoose');
+//1- Create Shema
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Name of user is required'],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    email: {
+      unique: true,
+      type: String,
+      trim: true,
+      required: [true, 'Email of user is required'],
+      lowercase: true,
+    },
+    phone: String,
+    profileImg: 'string',
+    password: {
+      type: String,
+      required: [true, 'Email of user is required'],
+      minlingth: [8, 'To short password'],
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+  },
+  { timestamps: true }
+);
+
+//2-export model
+
+const User =mongoose.model('User' ,userSchema);
+
+module.exports =User;
