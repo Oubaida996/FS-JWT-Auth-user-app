c617e47604a37625fc09d5bc0edef638245883e2
Obieda Akilan <obieda.jehad@gmail.com>
2023-03-05 12:26:27 +0300

    Set false for the strict query within dbConnction function

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,11 +2,13 @@
 const mongoose = require('mongoose');
 
 const dbConnection = () => {
+  // https://www.mongodb.com/community/forums/t/deprecationwarning-mongoose-the-strictquery/209637
+  mongoose.set('strictQuery', false);
   mongoose
     .connect(process.env.DB_URI, { dbName: process.env.DB_NAME })
     .then((conn) => {
       console.log('Database connected :' + conn.connection.host);
-    }); 
+    });
 };
 
 module.exports = dbConnection;
