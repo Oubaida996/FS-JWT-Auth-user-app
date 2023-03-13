919a096bee4d250ac58f7012b0b4bafb5e5b5d9c
Obieda Akilan <obieda.jehad@gmail.com>
2023-03-13 14:20:51 +0300

    Refactor the App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import Login from './container/login/Login';
+import React from 'react';
 import Main from './container/main/Main';
-import Register from './container/register/Register';
 
 function App() {
   return (
